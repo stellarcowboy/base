f0c20f0004b9c7cbf14be061f564ea8b19a08acf
sprclldr <kenneth@sprclldr.com>
2014-02-11 14:44:30 -0600

    Typo in charset of header

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 	<!-- While these meta keywords are not ideal and the meta description could be better, they are better than nothing -->
 	<meta name="keywords" content="<?php bloginfo( 'name' ); echo ' , '; bloginfo( 'description' ); ?>" />
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="index" title="<?php bloginfo( 'name' ); ?>" href="<?php echo get_option('home'); ?>/" />
