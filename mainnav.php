14148a585f778da91ffde03524da2768bac18765
sprclldr <kenneth@sprclldr.com>
2014-04-09 13:21:47 -0500

    Changed nav menu CSS from id to class.

--- mainnav.php
+++ mainnav.php
@@ -1,4 +1,4 @@
 <?php 
-wp_nav_menu( array('menu' => 'Header Menu','container' => 'div','container_id' => 'nav','depth' => 1 ));
+wp_nav_menu( array('menu' => 'Header Menu','container' => 'div','container_class' => 'nav header-menu','depth' => 1 ));
 // Insert code for reusable navigation, then include this file in templates.
 ?>
